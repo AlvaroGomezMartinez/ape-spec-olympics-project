78ce5dd2ee6e6ab449eec2b9822a431bd5f98163
Alvaro Gomez <alvaro.gomez2011@gmail.com>
2024-04-18 16:47:42 -0500

    Cleaned up the day sheets function.

--- Create Day Sheets.js
+++ Create Day Sheets.js
@@ -12,9 +12,6 @@ function createDaySheets() {
     switch (value[0]) {
       case 1:
         day1.push(value);
-
-          // insert function to push events to day1 array
-
         break;
       case 2:
         day2.push(value);

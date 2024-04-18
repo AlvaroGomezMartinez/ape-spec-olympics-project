05db3a779ae881805ef10177f5996c7fcf72ccb5
Alvaro Gomez <alvaro.gomez2011@gmail.com>
2024-04-18 14:41:15 -0500

    Made updates

--- Push Event Data to Day Sheets.js
+++ Push Event Data to Day Sheets.js
@@ -1,16 +1,2 @@
 // This function gets the data from 'Student Database' and filters it by event.
 
-
-
-
-
-function pushEventDataToDaySheet(tAndFEventDay, athGender, trackEvent) {
-    
-    let filteredData = [];
-    for (let i = 1; i < dataLength; i++) {
-      let row = data[i];
-      if (row[0] === tAndFEventDay && row[3] === athGender && row[8] === true && row[11] === trackEvent) {
-        filteredData.push(row);
-      }
-    }
-}
